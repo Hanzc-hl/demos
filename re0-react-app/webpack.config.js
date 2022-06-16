cc0f4e6d48f4e228a4497f087a9fd80a13d2b581
hanzc <1297411677@qq.com>
2022-06-16 16:25:49 +0800

    feat: 使用terser插件压缩打包, 并移除license文件

--- re0-react-app/webpack.config.js
+++ re0-react-app/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const FriendlyErrorsWebpackPlugin = require("@soda/friendly-errors-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
 
 const mockServer = require("./mock/server");
 
@@ -42,7 +43,18 @@ module.exports = {
           minChunks: 2
         }
       }
-    }
+    },
+    minimize: true,
+    minimizer: [
+      new TerserWebpackPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          }
+        },
+        extractComments: false,
+      })
+    ]
   },
   devServer: {
     setupMiddlewares: (middlewares, devServer) => {
